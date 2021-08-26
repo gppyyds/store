e6344c241e40be28fbe8f35c9c511275f6f45780
gppyyds <89378435+gppyyds@users.noreply.github.com>
2021-08-26 11:28:31 +0800

    Add files via upload

---
+++ g1.py
@@ -0,0 +1,83 @@
+# a=0
+# i=1
+# while  i<=10:
+#     print("请输入第",i,"个数")
+#     num =input()
+#     num =int(num)
+#     a+=num
+#     i+=1
+# print("总和:",a)
+
+# import random
+# num =random.randint(0,2)
+# print(num)#随机打印一个数
+# i=int(100)
+# print('猜猜我心中的数字（0-100）')
+# while 1:
+#     i =i-10
+#     a=input("请输入一个数字")#从键盘输入
+#     a=int(a)
+#     if a == num:
+#         print('恭喜你猜对了,游戏结束',i)
+#         break
+#     else:
+#         if i==0:
+#             print("提交失败")
+#             break
+#         else:
+#             print("回答错误，请再次猜数",i)
+
+
+/
+# import random
+# num =random.randint(50,150)
+# print(num)
+
+
+# a =int(input("请输入第一条的边长"))
+# b =int(input("请输入第二条边长"))
+# c =int(input("请输入第三条边边长"))
+# if   a==b ==c:
+#     print("为等边三角形")
+# elif a == b != c or a !=b==c  :
+#     print("为等腰三角形")
+# elif a*a+b*b ==c*c or b*b+c*c==a*a  or a*a+c*c==b*b :
+#     print('为直角三角形')
+# elif a +b >c or a +c >b or b +c >a :
+#     print("可以构成三角形")
+# elif a +b <c or a+c <b or b +c <a :
+#     print("不能构成三角形")
+
+# a =78
+# b =56
+# A =a +b -a
+# B =a +b -b
+# print(A)
+# print(B)
+
+
+
+# i =1
+# input ("用户名：root")
+# while i <=3:
+#     i =i +1
+#     g =input("请输入密码:admin")
+#     if   g=="admin":
+#         print("输入密码成功")
+#         break
+#     else:
+#         print("输入密码失败"
+
+#
+# print("        *")
+# print("       * *")
+# print("      * * *")
+# print("     * * * *")
+# print("    * * * * *")
+# print("   * * * * * *")
+# print("  * * * * * * *")
+
+
+a =3
+b =-2
+while
