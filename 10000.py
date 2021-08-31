a5de76556a20ff610481e01476d9df5e68a218b2
gppyyds <89378435+gppyyds@users.noreply.github.com>
2021-08-31 16:15:23 +0800

    Add files via upload

---
+++ 10000.py
@@ -0,0 +1,55 @@
+
+# list=[1,2,3,4,5,6,7,8,9]#通过角标来获取值
+# for i in range(len(list)):
+#     print(list[i])
+# dict={"河北":{"秦皇岛":{"北戴河":{"金港大道8号":"海牛"}}},
+#      "北京":{"昌平":{"沙河":{"七马路":"狼疼测试"}}}}
+# hebei=input("请输入您的地名")
+# if hebei=="河北":
+#     qinhuangdao=input("请输入您的地名")
+#     if qinhuangdao=="秦皇岛":
+#         beidaihe=input("请输入您的地名")
+#         if beidaihe=="北戴河":
+#             jingangdaodaobahao=input("请输入您的地名")
+#             if jingangdaodaobahao=="金港大道8号":
+#                 print(dict[hebei][qinhuangdao][beidaihe][jingangdaodaobahao])
+# else:
+#             if hebei=="北京":
+#                 qinhuangda = input("请输入您的地名")
+#                 if qinhuangda=="昌平":
+#                     beidaih = input("请输入您的地名")
+#                     if beidaih=="沙河":
+#                         jingangdadaobaha = input("请输入您的地名")
+#                         if jingangdadaobaha=="七马路":
+#                             print(dict[hebei][qinhuangda][beidaih][jingangdadaobaha])
+#
+
+dict={"k1":"v1","k2":"v2","k3":"v3"}
+# del dict#删除字典
+# print(dict)
+# del dict["k3"]#删除
+# print(dict)
+# dict["k1"]="s1"#更新替换
+# print(dict)
+# dict={"苹果":"32.8","香蕉":"22","葡萄":"15.5"}
+# dict["k4"]="v4"
+# print(dict)
+# print(dict.keys())
+# print(dict.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
